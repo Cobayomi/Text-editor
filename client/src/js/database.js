81fada859b1e6aab1afcdd11d7742f6810d6dc53
capricia <capriciaw16@gmail.com>
2023-03-19 20:36:51 -0500

    finishing todos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const jate = await openDB('jate', 1)
+  const get = jate.transaction('jate', 'readwrite')
+  const database = get.objectStore('jate')
+  const request = database.put({id: 1, value: content})
+  const result = await request
+  console.log(result)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  return await store.getAll();
+};
 
 initdb();
